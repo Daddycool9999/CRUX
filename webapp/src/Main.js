1332181fac5fdda5d0a6ff576b24cb9dcc30e310
Daddycool9999 <shivamnayak25.sn@gmail.com>
2020-10-11 16:32:39 +0530

    DONE

--- webapp/src/Main.js
+++ webapp/src/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from 'axios';
  
 class Main extends Component {
     constructor() {
@@ -7,25 +8,54 @@ class Main extends Component {
           textAreaValue: ""
         };
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit= this.handleSubmit.bind(this);
+      }
+      handleSubmit(event) {
+       
+        // console.log("HERE GO:");
+        // console.log(this.state.value);
+        
+        axios 
+        .post("http://localhost:8000/wel/", { 
+            text: this.state.value,
+        }) 
+        .then((res) => { 
+            // console.log(res.data);
+            this.setState({ 
+              value: res.data['text']
+            }); 
+        }) 
+        .catch((err) => {
+          console.log("ERROR");
+        }); 
+        event.preventDefault();
       }
     
       handleChange(event) {
-        this.setState({ textAreaValue: event.target.value });
+        this.setState({ value: event.target.value });
+        
+        // this.state.textAreaValue=this.state.value;
+        // console.log(this.state.value);
       }
+      
+
     render() {
     return (
         <div id="crux">
           <h1 >CRUX</h1>
-        <div id="upload">
-        
+      
+        <form id="para" onSubmit={this.handleSubmit}>  
         <textarea
-          value={this.state.textAreaValue}
+          value={this.state.value}
           onChange={this.handleChange}
           rows={50}
           cols={250}
+        
         />
-        <button>Summarise</button>
-      </div>
+         <input type="submit" value="Summarise" />
+        </form>
+       
+     
         </div>
         
     );
